432bb7248293ff7ff9af6b1c5cbfc97a9a67f0de
Fellipe Callegas <fellipe.callegas@withclutch.com>
2021-12-07 20:38:26 -0300

    feature signup

--- nestjs-task-management/src/auth/auth.controller.ts
+++ nestjs-task-management/src/auth/auth.controller.ts
@@ -1,5 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthCredentialsDto } from '../tasks/dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
+
+  @Post('/signup')
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
 }
--- nestjs-task-management/src/auth/auth.service.ts
+++ nestjs-task-management/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from '../tasks/dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,9 @@ export class AuthService {
     private usersRepository: UsersRepository,
   ) {
   }
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
+
 }
