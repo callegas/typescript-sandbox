0e8bb80ef5bffd406169e4918b856f7e7947a071
Fellipe Callegas <fellipe.callegas@withclutch.com>
2021-12-07 20:25:10 -0300

    Setting up Auth module

---
+++ nestjs-task-management/src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {
+  }
+}
--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -11,13 +11,13 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
-import { TaskStatus } from './task-status.enum';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {
+  }
 
   @Get()
   getTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
