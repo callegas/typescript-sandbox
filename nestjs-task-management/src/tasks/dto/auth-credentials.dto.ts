3d03c759897505b3b5316cf91feafee0a812162b
Fellipe Callegas <fellipe.callegas@withclutch.com>
2021-12-07 22:25:42 -0300

    Handle username conflicts

--- nestjs-task-management/src/tasks/dto/auth-credentials.dto.ts
+++ nestjs-task-management/src/tasks/dto/auth-credentials.dto.ts
@@ -9,9 +9,9 @@ export class AuthCredentialsDto {
   @IsString()
   @MinLength(8)
   @MaxLength(32)
-  @Matches(/((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message:
-      'password neest at least 1 upper case letter, 1 lower case letter and 1 number or special character',
+      'password needs at least 1 upper case letter, 1 lower case letter and 1 number or special character',
   })
   password: string;
 }
